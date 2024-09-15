002577545c1f92fe5b21220191f5fecc3e5cedbf
Manish Sonawane <manishsonawane2424@gmail.com>
2024-09-16 01:15:04 +0530

    course section done(comment)

--- landing-page-1/src/InstructorDashboard/CourseCard.js
+++ landing-page-1/src/InstructorDashboard/CourseCard.js
@@ -1,18 +1,18 @@
-// // src/components/CourseCard.js
-// import React from 'react';
-// import './CourseCard.css'; // For styling
+// src/components/CourseCard.js
+import React from 'react';
+import './CourseCard.css'; // For styling
 
-// const CourseCard = ({ image, title, description }) => {
-//   return (
-//     <div className="course-card">
-//       <img src={image} alt={title} className="course-image" />
-//       <div className="course-info">
-//         <h3>{title}</h3>
-//         <p>{description}</p>
-//       </div>
-//       <button className="course-button">View More</button>
-//     </div>
-//   );
-// };
+const CourseCard = ({ image, title, description }) => {
+  return (
+    <div className="course-card">
+      <img src={image} alt={title} className="course-image" />
+      <div className="course-info">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+      <button className="course-button">View More</button>
+    </div>
+  );
+};
 
-// export default CourseCard;
+export default CourseCard;
